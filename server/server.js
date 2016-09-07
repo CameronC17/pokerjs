d42bd1fecf943b501bb7aa8d2997de06c2dce539
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-07 11:07:19 +0100

    got the client connected to the server

---
+++ server/server.js
@@ -0,0 +1,68 @@
+var util 	= require('util');
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+//Game variables
+var endGame = false;
+var myPort = 8080;
+var gameTick = 20;
+var deck = [];
+var maxPlayers = 8;
+var clients = [];
+var rooms = [];
+
+
+//Client connection functions
+io.on('connection', onConnect);
+
+function onConnect(socket) {
+	console.log("\n + New connection. Reference: " + socket.id);
+	clients.push([socket.id, '']);
+	console.log("There are currently " + clients.length + " users online.");
+
+	//Functions here can only be ran once user is connected
+	socket.on("disconnect", onDisconnect);
+};
+
+function onDisconnect() {
+  clients.splice(clientPosByID(this.id), 1);
+  console.log("\n - Disconnected. Reference: " + this.id);
+};
+
+//Server start functions
+server.listen(process.env.PORT || myPort);
+console.log("\n\n----------------------\nServer started.\nListening on PORT: " + myPort);
+//DB CONNECT HERE
+//console.log('Database connection: Successful.');
+console.log("Starting game engine..." + "\n----------------------");
+game();
+//sendUpdate();
+
+
+//Game engine
+function game() {
+	//if (playersInGame.length > 0)
+	{
+		//do the things
+
+  	if (!endGame)
+  	{
+  		setTimeout(function () {
+  			game();
+      }, gameTick);
+  	}
+  };
+}
+
+
+//Functions for finding clients
+function clientPosByID(id) {
+  for (var i = 0; i < clients.length; i++) {
+    if (clients[i][0] == id) {
+      return id;
+    }
+  }
+}
