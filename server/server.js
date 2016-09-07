979fdb4726807b0435db664a27bf68a8db17e99b
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-07 11:29:28 +0100

    moved to client holder and client classes

--- server/server.js
+++ server/server.js
@@ -5,34 +5,75 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
-//Game variables
+//Classes /////////////////////////////////////////////////////////////////////////////////////////////
+class ClientHolder {
+  constructor() {
+    this.clients = [];
+  }
+
+  addClient(client) {
+    this.clients.push(client);
+  }
+
+  removeClient(id) {
+    this.clients.splice(this.findClientByID(this.id), 1);
+  }
+
+  findClientByID(id) {
+    for (var i = 0; i < this.clients.length; i++) {
+      if (this.clients[i].getID() == id) {
+        return id;
+      }
+    }
+  }
+
+  getConnected() {
+    return this.clients.length;
+  }
+}
+
+class Client {
+  constructor(id) {
+    this.clientID = id;
+  }
+
+  getID() {
+    return this.clientID;
+  }
+
+}
+
+//Game variables //////////////////////////////////////////////////////////////////////////////////////
 var endGame = false;
 var myPort = 8080;
 var gameTick = 20;
 var deck = [];
 var maxPlayers = 8;
-var clients = [];
+var clients = new ClientHolder();
 var rooms = [];
-
 
 //Client connection functions
 io.on('connection', onConnect);
 
 function onConnect(socket) {
 	console.log("\n + New connection. Reference: " + socket.id);
-	clients.push([socket.id, '']);
-	console.log("There are currently " + clients.length + " users online.");
+	//clients.push([socket.id, clients.length + 1]);
+  var newClient = new Client(socket.id);
+  clients.addClient(newClient);
+	console.log("There are currently " + clients.getConnected() + " users online.");
 
 	//Functions here can only be ran once user is connected
 	socket.on("disconnect", onDisconnect);
 };
 
 function onDisconnect() {
-  clients.splice(clientPosByID(this.id), 1);
+  //clients.splice(clientPosByID(this.id), 1);
+  clients.removeClient(this.id);
   console.log("\n - Disconnected. Reference: " + this.id);
+  console.log("There are currently " + clients.getConnected() + " users online.");
 };
 
-//Server start functions
+//Server start functions ////////////////////////////////////////////////////////////////////////////////
 server.listen(process.env.PORT || myPort);
 console.log("\n\n----------------------\nServer started.\nListening on PORT: " + myPort);
 //DB CONNECT HERE
@@ -42,7 +83,7 @@ game();
 //sendUpdate();
 
 
-//Game engine
+//Game engine //////////////////////////////////////////////////////////////////////////////////////////
 function game() {
 	//if (playersInGame.length > 0)
 	{
@@ -55,14 +96,4 @@ function game() {
       }, gameTick);
   	}
   };
-}
-
-
-//Functions for finding clients
-function clientPosByID(id) {
-  for (var i = 0; i < clients.length; i++) {
-    if (clients[i][0] == id) {
-      return id;
-    }
-  }
 }
